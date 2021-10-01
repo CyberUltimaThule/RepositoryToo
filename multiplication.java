3ccb1187a8d09ffe432af87dea691bd6ebf15514
CyberUltimaThule <77932850+CyberUltimaThule@users.noreply.github.com>
2021-10-01 08:07:13 +0300

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberA = in.nextInt();
+        int numberB = in.nextInt();
+        System.out.print(Math.pow(10, (Math.log10(numberA) + Math.log10(numberB))));
+        if (numberA == 0 || numberB == 0) {
+            System.out.print(0);
+        } else if (numberA == 1) {
+            System.out.print(numberB);
+        } else if (numberB == 1) {
+            System.out.print(numberA);
+        }
+    }
+}
