12b34bca7be0e0a0ec8c94f8cad39f7daf087af9
CyberUltimaThule <77932850+CyberUltimaThule@users.noreply.github.com>
2021-11-25 20:15:32 +0300

    Add files via upload

---
+++ AnimalThread.java
@@ -0,0 +1,35 @@
+package com.company;
+import java.lang.Thread;
+import java.lang.String;
+
+class AnimalThread extends Thread
+{
+    public static void main(String[] args){}
+    String Animal;
+    int Priority;
+    public AnimalThread(String Animal, int Priority) {
+        this.Priority = Priority;
+        this.Animal = Animal;
+    }
+
+
+    @Override
+    public void run() {
+        int rasstoyanie = 10;
+        Thread.currentThread().setName(Animal);
+        for(int metri = 1;metri <= rasstoyanie; metri ++){
+            try {
+                Thread.sleep(10);
+                System.out.println(Thread.currentThread().getName() + " пробежал(ла) " + metri + "метров");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }}}
+class RabbitAndTurtle {
+    public static void main(String[] args) {
+        AnimalThread rabbit = new AnimalThread("Кролик", Thread.MIN_PRIORITY);
+        AnimalThread turtle = new AnimalThread("Черепаха", Thread.MAX_PRIORITY);
+        turtle.start();
+        rabbit.start();
+    }
+}
