2583f4ade2657657646f4f1d8c1a7512c9df3c0f
CyberUltimaThule <77932850+CyberUltimaThule@users.noreply.github.com>
2021-12-23 08:50:33 +0300

    Параллельное скачивание и проигрывание музыки

---
+++ DownloadPicAndPlayMusic/JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.demo.downpicandmus","c":"DownPicPlayMusic","l":"DownPicPlayMusic()","u":"%3Cinit%3E()"},{"p":"ru.demo.downpicandmus","c":"DownPicPlayMusic","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
